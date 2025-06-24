b125e049fcafa76a1c3a8cd3d8b38c602636d648
Funbreak3 <floridaman520@gmail.com>
2025-06-24 12:50:05 -0500

    Change domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio-react/', // <--- ADD THIS LINE
+  base: '/', // <--- ADD THIS LINE
   plugins: [react()],
 })
